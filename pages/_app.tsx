b6a8e1c21c2953326dae619e05ed5ec17e44a641
kana-rus <kanarus786@gmail.com>
2022-03-23 23:49:23 +0900

    added LIFF SDK

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+
+import liff from '@line/liff'
+//import { Liff } from '@line/liff'
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // Execute liff.init() when the app is initialized
+  useEffect(() => {
+    // to avoid 'window is not defined' error
+    import('@line/liff').then(() => {
+      liff.init({ liffId: process.env.LIFF_ID! })
+    })
+  })
+
   return <Component {...pageProps} />
 }
 
