c8c8c4797a2b0fff07a5747e155499b9fd06c8b1
kana-rus <kanarus786@gmail.com>
2022-03-23 18:19:19 +0900

    implemented fontsize-adjusting system

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document = () => {
+  return (
+    <Html>
+      <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.15.3/dist/katex.min.css" integrity="sha384-KiWOvVjnN8qwAZbuQyWDIbfCLFhLXNETzBQjA/92pIowpC0d2O3nppDGQVgwd2nB" crossOrigin="anonymous"/>
+        
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
+export default Document
